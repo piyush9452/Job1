38b0ea4fd7bae9707f29abb2de761114318a6c1a
Kundan-Rawal <rawalkundan987@gmail.com>
2026-03-09 17:27:54 +0530

    changed the Gemini Model from 1.5 flash to 2.5 flash for compatiability issues

--- backend/controllers/aiControllers.js
+++ backend/controllers/aiControllers.js
@@ -11,7 +11,7 @@ export const generateJobDetails = expressAsyncHandler(async (req, res) => {
 
   // Initialize Gemini API
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
   const prompt = `
     You are an expert HR recruiter. Write a highly professional job description for a "${title}" position.
